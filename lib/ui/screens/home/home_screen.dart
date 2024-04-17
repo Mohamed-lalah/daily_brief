92cb0fb4c2247bce85546a10dc1d95902b220ec7
Mohamed-lalah <mohamedlalah918@gmail.com>
2024-04-18 01:53:51 +0200

    Drawer widget design and logic is done

--- lib/ui/screens/home/home_screen.dart
+++ lib/ui/screens/home/home_screen.dart
@@ -2,6 +2,7 @@ import 'package:daily_brief/data/api/api_manager.dart';
 import 'package:daily_brief/model/category_model.dart';
 import 'package:daily_brief/ui/screens/home/tabs/categories/categories_tab.dart';
 import 'package:daily_brief/ui/screens/home/tabs/news/news_tab.dart';
+import 'package:daily_brief/ui/screens/home/tabs/settings/setting_tab.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -23,29 +24,33 @@ class _HomeScreenState extends State<HomeScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return WillPopScope(
-      onWillPop: ()async{
-        if (currentTab is! CategoriesTab){
-          currentTab=CategoriesTab(categoryClick: onCategoryClick,) ;
-          setState(() {});
-          return false ;
-        }else {
-          return true;
-        }
+    return SafeArea(
+      child: WillPopScope(
+        onWillPop: ()async{
+          if (currentTab is! CategoriesTab){
+            currentTab=CategoriesTab(categoryClick: onCategoryClick,) ;
+            setState(() {});
+            return false ;
+          }else {
+            return true;
+          }
 
-      },
-      child: Scaffold(
-        appBar: AppBar(
-          centerTitle: true,
-          toolbarHeight: MediaQuery.of(context).size.height*0.09,
-          shape: const RoundedRectangleBorder(
-            borderRadius: BorderRadius.vertical(
-              bottom:Radius.circular(22)
-            )
+        },
+        child: Scaffold(
+          drawerEnableOpenDragGesture: false,
+          appBar: AppBar(
+            centerTitle: true,
+            toolbarHeight: MediaQuery.of(context).size.height*0.09,
+            shape: const RoundedRectangleBorder(
+              borderRadius: BorderRadius.vertical(
+                bottom:Radius.circular(22)
+              )
+            ),
+            title: Text("Daily Brief"),
           ),
-          title: Text("Daily Brief"),
+          drawer: buildDarwer(),
+          body: currentTab
         ),
-        body: currentTab
       ),
     );
   }
@@ -55,5 +60,50 @@ class _HomeScreenState extends State<HomeScreen> {
     setState(() {});
 
   }
+
+   Widget buildDarwer() {
+    return  Drawer(
+      child: Column(
+        children: [
+          const DrawerHeader(
+              decoration: BoxDecoration(
+                color: Color(0xff39A552)
+              ),
+              child: Center(child: Text("Daily Brief"))),
+
+
+          buildDrawerRow("Categories",Icons.list,(){
+            currentTab= CategoriesTab(categoryClick: onCategoryClick,);
+            setState(() {});
+            Navigator.pop(context);
+          }),
+          buildDrawerRow("Setting", Icons.settings,(){
+            currentTab= SettingTab();
+            setState(() {});
+            Navigator.pop(context);
+
+
+          })
+        ],
+      ),
+
+    );
+   }
+   Widget  buildDrawerRow(String title , IconData icon , Function onClick){
+    return InkWell(
+      onTap: (){
+        onClick();
+      },
+      child: Row(
+        children: [
+          Icon(icon,color: Colors.black ,size: 35,),
+          SizedBox(width: 8,),
+          Text(title),
+        ],
+      ),
+    );
+   }
+
+
 }
 
